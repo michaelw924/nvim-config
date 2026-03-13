63fe03670fca47b7acf2c287a9f739f869e79559
Michael White <michaelwhite924@gmail.com>
2026-03-12 22:44:50 -0500

    fixing nodejs installation

--- install.sh
+++ install.sh
@@ -6,16 +6,18 @@ echo "Installing Neovim environment..."
 
 ### Detect package manager
 
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+
 if command -v apt >/dev/null; then
     PKG_INSTALL="sudo apt install -y"
     sudo apt update
-    $PKG_INSTALL git curl build-essential unzip ripgrep fd-find npm xclip
+    $PKG_INSTALL git curl build-essential unzip ripgrep fd-find nodejs xclip
 elif command -v dnf >/dev/null; then
     PKG_INSTALL="sudo dnf install -y"
-    $PKG_INSTALL git curl gcc make unzip ripgrep fd-find npm xclip
+    $PKG_INSTALL git curl gcc make unzip ripgrep fd-find nodejs xclip
 elif command -v pacman >/dev/null; then
     PKG_INSTALL="sudo pacman -S --noconfirm"
-    $PKG_INSTALL git curl gcc make unzip ripgrep fd npm xclip
+    $PKG_INSTALL git curl gcc make unzip ripgrep fd nodejs xclip
 else
     echo "Unsupported package manager"
     exit 1
