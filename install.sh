c339020468239688cdf00cfc810c124253f46d1f
Michael White <michaelwhite924@gmail.com>
2026-03-12 22:50:52 -0500

    sudo for npm

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ sudo ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
 ### Install tree-sitter CLI (correct version)
 
 echo "Installing tree-sitter CLI..."
-npm install -g tree-sitter-cli
+sudo npm install -g tree-sitter-cli
 
 ### Install config
 
